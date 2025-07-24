24590b5b3c4669abcdecc343b9ad8bc380cf1740
LuisLaguardia-dev <luislaguardiadev@gmail.com>
2025-07-24 20:12:55 +0200

    fifth commit

---
+++ EldritchHollow/assets/maps/LittleForestTreeSprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="LittleForestTreeSprites" tilewidth="15" tileheight="15" tilecount="100" columns="10">
+ <image source="../../../../../TFG/Recursos TFG/TileSet/LittleForestTreeSprites.png" width="160" height="160"/>
+</tileset>
